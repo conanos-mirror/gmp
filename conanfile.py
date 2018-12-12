579e2961c2f202cf32fe68f029a44d919239f3ab
cppbitman <daihongjun@kedacom.com>
2018-12-12 15:22:07 +0800

    windows build done

--- conanfile.py
+++ conanfile.py
@@ -1,125 +1,121 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, tools, AutoToolsBuildEnvironment,CMake
+from conans import ConanFile, tools, AutoToolsBuildEnvironment,MSBuild
 import os
-
-from conanos.build import config_scheme,pkgconfig_adaption
-
-def _abspath(folder):
-    return os.path.abspath(folder).replace('\\','/')
+from conanos.build import config_scheme
 
 class GmpConan(ConanFile):
     name = "gmp"
-    version = "6.1.2"
-    url = "https://github.com/bincrafters/conan-gmp"
+    version = "6.1.2-5"
     description = "GMP is a free library for arbitrary precision arithmetic, operating on signed integers, rational numbers, and floating-point numbers."
-    website = "https://gmplib.org"
+    url = "https://github.com/conanos/gmp"
+    homepage = "https://gmplib.org"
     license = "LGPL-3.0, GPL-2.0"
-    exports = ["LICENSE.md"]
-    exports_sources = ["FindGMP.cmake",'CMakeLists.txt','cmake/*']
-    source_subfolder = "source_subfolder"
+    exports = ["COPYINGv3","COPYINGv2"]
+    generators = "visual_studio", "gcc"
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False], "disable_assembly": [True, False]}#, "run_checks": [True, False]}
-    default_options = "shared=False", "fPIC=True", "disable_assembly=True"#, "run_checks=False"
-    generators = "cmake"
+    default_options = { 'shared': True, 'fPIC': True, 'disable_assembly' : False } #, "run_checks=False"
 
-    _source_folder    ='_source'
-    _pkgconfig_folder ='_pkgconfig'
-    _build_folder     ='_build'
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
 
-    @property
-    def is_msvc(self):
-        return self.settings.compiler == "Visual Studio"
-
-    @property
-    def run_checks(self):
-        CONANOS_RUN_CHECKS = os.environ.get('CONANOS_RUN_CHECKS')
-        if CONANOS_RUN_CHECKS:
-            return self.name in CONANOS_RUN_CHECKS.split()
-        return False
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
 
     def configure(self):
-        # Because this is pure C
         del self.settings.compiler.libcxx
-        if self.is_msvc:
-            del self.options.fPIC
-            if self.options.shared:
-               raise tools.ConanException("The gmp package cannot be built shared on Visual Studio.")
-               
-    def requirements(self):
         config_scheme(self)
 
     def source(self):
-        source_url = "https://gmplib.org/download/gmp"
-        tools.get("{0}/{1}-{2}.tar.bz2".format(source_url, self.name, self.version))
+        url_ = "https://github.com/ShiftMediaProject/gmp/archive/{version}.tar.gz".format(version=self.version)
+        tools.get(url_)
         extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_folder)
+        os.rename(extracted_dir, self._source_subfolder)
 
-    def autotool_build(self):
-        env_build = AutoToolsBuildEnvironment(self)
-        env_build.fpic = self.options.fPIC
-        with tools.environment_append(env_build.vars):
+    #def autotool_build(self):
+    #    env_build = AutoToolsBuildEnvironment(self)
+    #    env_build.fpic = self.options.fPIC
+    #    with tools.environment_append(env_build.vars):
 
-            with tools.chdir(self._source_folder):
+    #        with tools.chdir(self._source_folder):
 
-                if self.settings.os == "Macos":
-                    tools.replace_in_file("configure", r"-install_name \$rpath/", "-install_name ")
+    #            if self.settings.os == "Macos":
+    #                tools.replace_in_file("configure", r"-install_name \$rpath/", "-install_name ")
 
-                self.run("chmod +x configure")
+    #            self.run("chmod +x configure")
 
-                configure_args = []
-                if self.options.disable_assembly:
-                    configure_args.append('--disable-assembly')
-                if self.options.shared:
-                    configure_args.extend(["--enable-shared", "--disable-static"])
-                else:
-                    configure_args.extend(["--disable-shared", "--enable-static"])
+    #            configure_args = []
+    #            if self.options.disable_assembly:
+    #                configure_args.append('--disable-assembly')
+    #            if self.options.shared:
+    #                configure_args.extend(["--enable-shared", "--disable-static"])
+    #            else:
+    #                configure_args.extend(["--disable-shared", "--enable-static"])
 
-                env_build.configure(args=configure_args)
-                env_build.make()
+    #            env_build.configure(args=configure_args)
+    #            env_build.make()
 
-                # According to the gmp readme file, make check should not be omitted, but it causes timeouts on the CI server.
-                #if self.options.run_checks:
-                if self.run_checks:
-                    env_build.make(args=['check'])
-                env_build.install()
+    #            # According to the gmp readme file, make check should not be omitted, but it causes timeouts on the CI server.
+    #            #if self.options.run_checks:
+    #            if self.run_checks:
+    #                env_build.make(args=['check'])
+    #            env_build.install()
 
-    def cmake_build(self):
-        GMP_PROJECT_DIR = os.path.abspath(self._source_folder).replace('\\','/')
-        
-        cmake = CMake(self)
-        cmake.configure(source_folder= '.',build_folder='~build',
-            defs={'USE_CONAN_IO':True,
-                  'GMP_PROJECT_DIR':GMP_PROJECT_DIR,
-                  'ENABLE_UNIT_TESTS':self.run_checks
-            })
-        cmake.build()
-        if self.run_checks:
-            cmake.test()
-        cmake.install()
+    #def cmake_build(self):
+    #    GMP_PROJECT_DIR = os.path.abspath(self._source_folder).replace('\\','/')
+    #    
+    #    cmake = CMake(self)
+    #    cmake.configure(source_folder= '.',build_folder='~build',
+    #        defs={'USE_CONAN_IO':True,
+    #              'GMP_PROJECT_DIR':GMP_PROJECT_DIR,
+    #              'ENABLE_UNIT_TESTS':self.run_checks
+    #        })
+    #    cmake.build()
+    #    if self.run_checks:
+    #        cmake.test()
+    #    cmake.install()
 
+    #def build(self):
+    #    #pkgconfig_adaption(self,_abspath(self._source_folder))
+    #    
+    #    if self.is_msvc:
+    #        self.cmake_build()
+    #    else:
+    #        self.autotool_build()
+    
     def build(self):
-        pkgconfig_adaption(self,_abspath(self._source_folder))
-        
-        if self.is_msvc:
-            self.cmake_build()
-        else:
-            self.autotool_build()
+        if self.settings.os == "Windows":
+            with tools.chdir(os.path.join(self._source_subfolder,"SMP")):
+                msbuild = MSBuild(self)
+                build_type = str(self.settings.build_type) + ("DLL" if self.options.shared else "")
+                msbuild.build("libgmp.sln",upgrade_project=True, build_type=build_type)
 
+
+    #def package(self):
+    #    return
+    #    # dual license
+    #    self.copy("COPYINGv2", dst="licenses", src=self._source_folder)
+    #    self.copy("COPYING.LESSERv3", dst="licenses", src=self._source_folder)
+    #    self.copy(pattern="gmp.h", dst="include", src=self._source_folder)
+    #    self.copy("FindGMP.cmake")
+    #    if self.options.shared:
+    #        self.copy(pattern="libgmp.so*", dst="lib", src="%s/.libs" % self._source_folder, keep_path=False)
+    #        self.copy(pattern="libgmp.dylib", dst="lib", src="%s/.libs" % self._source_folder, keep_path=False)
+    #        self.copy(pattern="libgmp.*.dylib", dst="lib", src="%s/.libs" % self._source_folder, keep_path=False)
+    #    else:
+    #        self.copy(pattern="libgmp.a", dst="lib", src="%s/.libs" % self._source_folder, keep_path=False)
+    
     def package(self):
-        return
-        # dual license
-        self.copy("COPYINGv2", dst="licenses", src=self._source_folder)
-        self.copy("COPYING.LESSERv3", dst="licenses", src=self._source_folder)
-        self.copy(pattern="gmp.h", dst="include", src=self._source_folder)
-        self.copy("FindGMP.cmake")
-        if self.options.shared:
-            self.copy(pattern="libgmp.so*", dst="lib", src="%s/.libs" % self._source_folder, keep_path=False)
-            self.copy(pattern="libgmp.dylib", dst="lib", src="%s/.libs" % self._source_folder, keep_path=False)
-            self.copy(pattern="libgmp.*.dylib", dst="lib", src="%s/.libs" % self._source_folder, keep_path=False)
-        else:
-            self.copy(pattern="libgmp.a", dst="lib", src="%s/.libs" % self._source_folder, keep_path=False)
+        if self.settings.os == "Windows":
+            platform = {'x86': 'x86','x86_64': 'x64'}
+            rplatform = platform.get(str(self.settings.arch))
+            self.copy("*", dst=os.path.join(self.package_folder,"include"), src=os.path.join(self.build_folder,"..", "msvc","include"))
+            for i in ["lib","bin"]:
+                self.copy("*", dst=os.path.join(self.package_folder,i), src=os.path.join(self.build_folder,"..","msvc",i,rplatform))
+            self.copy("*", dst=os.path.join(self.package_folder,"licenses"), src=os.path.join(self.build_folder,"..", "msvc","licenses"))
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
